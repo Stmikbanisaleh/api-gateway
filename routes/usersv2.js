48893034ab69d53fc08a20bc057dcd6b334648a0
imamsatrianta <imamsatrianta@gmail.com>
2019-12-07 17:44:39 +0700

    update reset passwrod

--- routes/usersv2.js
+++ routes/usersv2.js
@@ -233,4 +233,54 @@ router.post('/getuserbyid', (req, res) => {
   });
 }
 });
+
+router.post('/getuserbyemail', (req, res) => {
+    UserSchema.findAll({
+      where: {
+        email: req.body.email,
+      },
+    })
+      .then((data) => {
+        if (data.length < 1) {
+          res.status(404).json({
+            message: 'Not Found',
+          });
+        } else {
+          res.status(200).json(data);
+        }
+        // });x
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+          status: 500,
+        });
+      });
+});
+
+router.post('/inserttoken', (req, res) => {
+    UserSchema.create({
+      email: req.body.email,
+      token: req.body.token,
+      data_created: req.body.token
+    })
+      .then((data) => {
+        if (data.length < 1) {
+          res.status(404).json({
+            message: 'Not Found',
+          });
+        } else {
+          res.status(200).json(data);
+        }
+        // });x
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+          status: 500,
+        });
+      });
+
+});
+
 module.exports = router;
